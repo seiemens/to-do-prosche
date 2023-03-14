b7c21143a094bc0ce4ad9277b4afbebe098e7800
Schwaen <svenme24@gmail.com>
2023-03-14 16:08:22 +0100

    fix: removed .vscode folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ViewTodosComponent } from './components/view-todos/view-todos.component';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
+import { TodosPanelComponent } from './components/todos-panel/todos-panel.component';
+import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     LoginComponent,
+    AdminPanelComponent,
+    SignupComponent,
+    ViewTodosComponent,
+    UserPanelComponent,
+    TodosPanelComponent,
+    CreateTodoComponent,
+    EditTodoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/create-todo/create-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-todo',
+  templateUrl: './create-todo.component.html',
+  styleUrls: ['./create-todo.component.scss']
+})
+export class CreateTodoComponent {
+
+}
